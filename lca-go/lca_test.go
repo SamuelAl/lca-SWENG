12194967808b7147de2417cd3bf49dff3361efcc
SamuelAl <s.alarco@outlook.com>
2021-10-11 08:20:12 +0100

    Added degenerate tree tests for Go

--- lca-go/lca_test.go
+++ lca-go/lca_test.go
@@ -53,3 +53,52 @@ func TestMultiNodeTree(t *testing.T) {
 		})
 	}
 }
+
+func TestDegenerateTree(t *testing.T) {
+	root := createDegenerateTree()
+
+	tests := []struct {
+		name    string
+		nodeA   int
+		nodeB   int
+		wantLCA int
+	}{
+		{
+			name:    "Root is LCA of itself and descendant",
+			nodeA:   0,
+			nodeB:   1,
+			wantLCA: 0,
+		},
+		{
+			name:    "Root is LCA of itself and descendants",
+			nodeA:   2,
+			nodeB:   0,
+			wantLCA: 0,
+		},
+		{
+			name:    "1 is LCA of itself and direct descendant",
+			nodeA:   1,
+			nodeB:   2,
+			wantLCA: 1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got := FindLCA(root, test.nodeA, test.nodeB).node.value; got != test.wantLCA {
+				t.Fatalf("FindLCA(%v, %d, %d): got = %d, want = %d", root, test.nodeA, test.nodeB, got, test.wantLCA)
+			}
+		})
+	}
+}
+
+func createDegenerateTree() *TreeNode {
+	root := NewTreeNode(0)
+	child1 := NewTreeNode(1)
+	child2 := NewTreeNode(2)
+
+	root.AddChild(child1)
+	child1.AddChild(child2)
+
+	return root
+}
