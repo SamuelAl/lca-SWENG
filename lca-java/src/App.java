a2d773572b4bcc59030866ef509c2364bae6ba45
einstenz <33717014+einstenz@users.noreply.github.com>
2021-10-26 12:27:01 +0100

    Removed unnecessary classes and code no longer valid.

--- lca-java/src/App.java
+++ lca-java/src/App.java
@@ -1,26 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Finding LCA!");
-
-        TreeNode tree = createSampleTree();
-        LCA lca = new LCA(3, 5, tree);
-        System.out.printf("LCA(3,5) = %d", lca.getLCA().value);
-    }
-
-    private static TreeNode createSampleTree() {
-        TreeNode root = new TreeNode(0);
-        TreeNode child1 = new TreeNode(1);
-        TreeNode child2 = new TreeNode(2);
-        TreeNode child3 = new TreeNode(3);
-        TreeNode child4 = new TreeNode(4);
-        TreeNode child5 = new TreeNode(5);
-
-        root.addChild(child1);
-        root.addChild(child2);
-        child1.addChild(child3);
-        child1.addChild(child4);
-        child1.addChild(child5);
-
-        return root;
     }
 }
--- lca-java/src/LCA.java
+++ lca-java/src/LCA.java
@@ -1,42 +1,9 @@
 public class LCA {
-    private int nodeA;
-    private int nodeB;
-    private TreeNode root;
+   
 
-    private TreeNode solution;
-
-    public LCA(int nodeA, int nodeB, TreeNode root) {
-        if (root == null) throw new IllegalArgumentException("Must provide a valid root");
-        this.nodeA = nodeA;
-        this.nodeB = nodeB;
-        this.root = root;
-    }
-
-    public TreeNode getLCA() {
-        if (solution != null) return solution;
-        getLCARecursive(root);
-        return solution;
-    }
-
-    private int getLCARecursive(TreeNode curr) {
-        if (curr == null) return 0; 
-        int count = 0;
+    public LCA(int nodeA, int nodeB, EdgeWeightedDigraph root) {
         
-        if (curr.value == this.nodeA)  count++;
-        if (curr.value == this.nodeB)  count++;
-
-        if (count >= 2 && solution == null) {
-            solution = curr;
-            return count;
-        }
-
-        for (TreeNode child : curr.children) {
-            count += getLCARecursive(child);
-            if (count >= 2 && solution == null) {
-                solution = curr;
-                return count;
-            }
-        }
-        return count;
     }
+
+    
 }
