80628c9c06ffcc16461a5cbb71f420a19e37d0da
einstenz <33717014+einstenz@users.noreply.github.com>
2021-10-26 13:02:10 +0100

    Added create test graph helper function.

--- lca-java/tests/LCATest.java
+++ lca-java/tests/LCATest.java
@@ -9,6 +9,36 @@ public class LCATest {
         new LCA(graph);
     }
 
-    
+    /**
+     * Creates sample digraph to be used in testing.
+     * @return Test graph.
+     */
+    public EdgeWeightedDigraph createTestDigraph() {
+        /**
+         * Test Graph:
+         * 
+         *            -->-- 3
+         *          /        \
+         * 1 -->-- 2           -->-- 5
+         *          \        /
+         *            -->-- 4 -->-- 8
+         *                 /
+         * 6 -->-- 7 -->-- 
+         * 
+         */
+
+        final int weight = 1; // Constant weight used.
+        
+        EdgeWeightedDigraph testGraph = new EdgeWeightedDigraph();
+        testGraph.addEdge(new DirectedEdge(1, 2, weight));
+        testGraph.addEdge(new DirectedEdge(2, 3, weight));
+        testGraph.addEdge(new DirectedEdge(2, 4, weight));
+        testGraph.addEdge(new DirectedEdge(3, 5, weight));
+        testGraph.addEdge(new DirectedEdge(4, 5, weight));
+        testGraph.addEdge(new DirectedEdge(4, 8, weight));
+        testGraph.addEdge(new DirectedEdge(6, 7, weight));
+        testGraph.addEdge(new DirectedEdge(7, 4, weight));
+        return testGraph;
+    }
    
 }
